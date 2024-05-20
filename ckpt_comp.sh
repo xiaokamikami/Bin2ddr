d58d049617d020a809fbcf3fd41b755e1bb0e080
xiaokamikami <fengkehan@bosc.ac.cn>
2024-05-20 17:34:01 +0800

    add exampl shell

--- ckpt_comp.sh
+++ ckpt_comp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #example: sh ckpt_comp.sh -g ../test/test.gz -r ./test/gcpt.bin  
-nemu="./nemu/riscv64-nemu-interpreter"
+nemu="./ready-to-run/riscv64-nemu-interpreter"
 compress="./out/compress-out.txt"
 while getopts n:c:r:g: opt
 do 
@@ -13,5 +13,5 @@ do
 done
 echo "gcpt: $gcpt";
 
-./nemu/riscv64-nemu-interpreter -b --restore $ckpt -r $gcpt -I 40000000 --mem_use_record_file $compress
+$nemu -b --restore $ckpt -r $gcpt -I 40000000 --mem_use_record_file $compress
 ./bin2ddr -i $ckpt -o ./out.dat -m "row,ba,col,bg" -r $gcpt -c $compress
