5a7e8d7389bc4da5c4689a3f64fc520fbf15f038
Kamimiao <fengkehan@bosc.ac.cn>
2024-12-26 17:38:43 +0800

    CI: Merge pr and push CI to increase the test of checkpoint (#8)
    * PRINT: Print more precise memory size information when converting
    
    CI: Merge pr and push CI to increase the test of checkpoint and release memory when the operation ends.

--- include/load.h
+++ include/load.h
@@ -10,5 +10,5 @@
 extern uint64_t *ram;
 uint64_t load_img(const char *image, const uint8_t ch_num, const uint8_t rank_num);
 uint64_t override_ram(const char* img_name, uint64_t over_size);
-
+void finish_ram();
 #endif
